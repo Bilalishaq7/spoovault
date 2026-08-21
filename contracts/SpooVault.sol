9839255ce506ea6f452ae109bcf495faf4a37027
GiftedGiftB <asekhamegift@gmail.com>
2026-08-21 01:14:11 +0100

    feat: implement ERC-6551 Token Bound Account support in SpooVault with registry and account implementation contracts

--- contracts/SpooVault.sol
+++ contracts/SpooVault.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./interfaces/IERC6551Registry.sol";
 
 /**
  * @title SpooVault
@@ -12,6 +13,9 @@ contract SpooVault is ERC721 {
     uint256 private _vaultIdCounter;
     uint256 private _documentIdCounter;
     uint256 private _requestIdCounter;
+
+    address public erc6551Registry;
+    address public tbaImplementation;
 
     enum RequestStatus {
         PENDING,
@@ -201,6 +205,32 @@ contract SpooVault is ERC721 {
     }
 
     constructor() ERC721("SpooVault Access Token", "SPVT") {}
+
+    /**
+     * @dev Initialize ERC-6551 Token Bound Account support.
+     * Can only be called once to set the registry and implementation addresses.
+     */
+    function initializeERC6551(address registry, address implementation) external {
+        if (erc6551Registry != address(0)) revert("ERC6551 already initialized");
+        erc6551Registry = registry;
+        tbaImplementation = implementation;
+    }
+
+    /**
+     * @dev Computes the deterministic Token Bound Account address for a given vault NFT.
+     */
+    function computeVaultAccount(uint256 tokenId) external view returns (address) {
+        if (erc6551Registry == address(0) || tbaImplementation == address(0)) {
+            revert("ERC6551 not initialized");
+        }
+        return IERC6551Registry(erc6551Registry).account(
+            tbaImplementation,
+            bytes32(0),
+            block.chainid,
+            address(this),
+            tokenId
+        );
+    }
 
     /**
      * @dev Create a new vault with guardian invites.
